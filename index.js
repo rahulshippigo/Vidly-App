5ba6dea028647ad11c457e72274f4029080b97d1
rahul <rahul.sharma@shippigo.com>
2021-05-08 18:19:42 +0530

    Vidly-App

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const genres = require('./routes/genres');
+const customers = require('./routes/customer');
+const movies = require('./routes/movies');
+const rentals = require('./routes/rentals');
+const app = express();
+
+const mongoOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+mongoose.connect('mongodb://localhost:27017/vidly', mongoOptions)
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.log('Could not connect to MongoDB....', err));
+
+app.use(express.json());
+app.use('/api/genres', genres);
+app.use('/api/customers', customers);
+app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
+
+console.log('Listeing on Port',  process.env.PORT);
+const port = process.env.PORT || 3000;
+// app.listen(port, () => console.log(`Listening on Port ${port}...`));
